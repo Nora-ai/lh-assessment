54ac5cba4e59853fa389dd1a3e15c415a0038419
snora <noratulchina@gmail.com>
2022-01-20 12:17:59 +0200

    added github link

--- lh-dashboard/components/Navbar.js
+++ lh-dashboard/components/Navbar.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Navbar = () => {
     return (
@@ -6,9 +7,11 @@ const Navbar = () => {
         <span>
           <Image src="/6119a8ddf9bf69a32c0806b3_Fill Dark.svg" alt="LH Logo" width={130} height={35} />
         </span>
-        <button className="nav-button">
+        <Link href="https://github.com/Nora-ai/lh-assessment">
+        <a target="_blank"><button className="nav-button">
             View Repo on Github
-        </button>
+        </button></a>
+        </Link>
   </div>
     )
 }
